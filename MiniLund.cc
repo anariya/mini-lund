f6285e516b8041e1f62d0a87fda705c93dcffdcc
Jade Abidi <jabi0002@student.monash.edu>
2025-08-26 19:58:11 +1000

    rename

---
+++ MiniLund.cc
@@ -0,0 +1,11 @@
+/*
+An instance of MiniLund is analagous to an instance of Pythia, providing an
+event record and a collection of particle data, and allowing for q-qbar
+hadronisation events to be generated.
+
+Author: Jade Abidi
+ */
+
+#include "MiniLund/MiniLund.h"
+
+namespace MiniLund
